8bdd115a78ff743c072a6395b84a6e4ea1f4ced2
Richard <rngaboyisonga@gmail.com>
2024-10-17 16:44:04 +0200

    updating again

--- app.py
+++ app.py
@@ -437,5 +437,4 @@ def internal_error(error):
  
 # Run the application
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=5000)
